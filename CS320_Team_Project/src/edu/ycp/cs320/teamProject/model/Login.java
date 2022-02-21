c9cdabe3b61f9bff9bd98bc7e78b090a2d98cb92
Brody Hageneder <bhageneder@ycp.edu>
2022-02-21 08:09:05 -0500

    Changes to Login model

--- CS320_Team_Project/src/edu/ycp/cs320/teamProject/model/Login.java
+++ CS320_Team_Project/src/edu/ycp/cs320/teamProject/model/Login.java
@@ -1,5 +1,31 @@
 package edu.ycp.cs320.teamProject.model;
 
 public class Login {
-
+	private String username, password;
+	
+	public Login()
+	{
+		username = null;
+		password = null;
+	}
+	
+	public void setUsername(String inputUsername)
+	{
+		this.username = inputUsername;
+	}
+	
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setPassword(String inputPassword)
+	{
+		this.password = inputPassword;
+	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
 }
