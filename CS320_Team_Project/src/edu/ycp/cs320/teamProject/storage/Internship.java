4e79c161a345ed91510ca4111464b67f81192470
Michael Geyer <mgeyer@ycp.edu>
2022-04-04 08:40:34 -0400

    Revert "Revert "Fake DB progress on hashing""
    This reverts commit 209c168c8bfa667987211dbb84084ef7ae265ff1.

--- CS320_Team_Project/src/edu/ycp/cs320/teamProject/storage/Internship.java
+++ CS320_Team_Project/src/edu/ycp/cs320/teamProject/storage/Internship.java
@@ -1,5 +1,16 @@
 package edu.ycp.cs320.teamProject.storage;
 
-public class Internship {
-
+// Inheritance Call
+public class Internship extends Information {
+	//Fields 
+	
+		private int length; 
+		private int resumeStrength; 
+		
+		
+	//Constructors 
+		public Internship(int length, int resumeStrength){
+			this.length = length;  
+			this.resumeStrength = resumeStrength; 
+		}
 }
