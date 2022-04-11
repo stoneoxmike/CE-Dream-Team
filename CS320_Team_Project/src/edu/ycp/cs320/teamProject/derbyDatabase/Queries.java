9596d2b58e422ca44d5bbfb09f1c7c98d80f22ef
bhageneder <bhageneder@ycp.edu>
2022-04-11 19:29:56 -0400

    Adjustment to Queries Extensions
    Queries shouldn't extend IDatabase because IDatabase is not an abstract class but rather have an instanced created for a reference to the Database Provider class which connects to the IDatabase as an instance.

--- CS320_Team_Project/src/edu/ycp/cs320/teamProject/derbyDatabase/Queries.java
+++ CS320_Team_Project/src/edu/ycp/cs320/teamProject/derbyDatabase/Queries.java
@@ -1,7 +1,11 @@
 package edu.ycp.cs320.teamProject.derbyDatabase;
 
-public class Queries implements IDatabase {
+import edu.ycp.cs320.teamProject.derbyDatabase.DatabaseProvider;
+import edu.ycp.cs320.teamProject.derbyDatabase.IDatabase;
+
+public class Queries{
 	static {
+		IDatabase db = DatabaseProvider.getInstance();
 		try {
 			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
 		} catch (Exception e) {
