73cd64d2bedc951a3c1fba210400190ab874d063
vpagano12 <vpagano1@ycp.edu>
2022-04-24 21:30:08 -0400

    Many new tests in co-op and information

--- CS320_Team_Project/junit/edu/ycp/cs320/teamProject/storage/CoopTest.java
+++ CS320_Team_Project/junit/edu/ycp/cs320/teamProject/storage/CoopTest.java
@@ -39,4 +39,41 @@ public Co_op test1, test2, test3, test4;
 		assertTrue(test1.getCredits() == 3);
 	}
 	
+	//Setting for new tests
+
+	public void testgetresumeStrengthset() throws Exception{
+		test1.setresumeStrength(3);
+		test2.setresumeStrength(5);
+		test3.setresumeStrength(1);
+		test4.setresumeStrength(2);
+		assertTrue(test1.getresumeStrength() == 3);
+		assertTrue(test2.getresumeStrength() == 5);
+		assertTrue(test3.getresumeStrength() == 1);
+		assertTrue(test4.getresumeStrength() == 2);
+	}
+	@Test
+	public void testgetLengthset() throws Exception{
+		test1.setLength(5);
+		test2.setLength(10);
+		test3.setLength(7);
+		test4.setLength(3);
+		assertTrue(test1.getLength() == 5);
+		assertTrue(test2.getLength() == 10);
+		assertTrue(test3.getLength() == 7);
+		assertTrue(test4.getLength() == 3);
+	}
+	@Test
+	public void testgetCreditsset() throws Exception{
+		test1.setCredits(4);
+		test2.setCredits(0);
+		test3.setCredits(2);
+		test4.setCredits(3);
+		assertTrue(test1.getCredits() == 4);
+		assertTrue(test2.getCredits() == 0);
+		assertTrue(test3.getCredits() == 2);
+		assertTrue(test4.getCredits() == 3);
+	}
+		
+		
+	
 }
--- CS320_Team_Project/junit/edu/ycp/cs320/teamProject/storage/InformationTest.java
+++ CS320_Team_Project/junit/edu/ycp/cs320/teamProject/storage/InformationTest.java
@@ -20,6 +20,10 @@ public class InformationTest {
     	 test4 = new Information(0, 0, 0, 0, 1, 0);
     	 general = new Information();
     	 general.setSalary(1010.10);
+    	 general.setHousingStipend(1600);
+    	 general.setLocation("detroit");
+    	 general.setCommuteTime(20.0);
+    	 general.setRemote(true);
     }
 
     @Test
@@ -36,11 +40,119 @@ public class InformationTest {
     public void testGetSalary() throws Exception {
     	assertTrue(general.getSalary() == 1010.10);
     }
+    
+    @Test
+    public void testHousingStipendy() throws Exception {
+    	assertTrue(general.getHousingStipend() == 1600);
+    }
+    
+    @Test
+    public void testgetLocation() throws Exception {
+    	assertTrue(general.getLocation().contentEquals("detroit"));
+    }
+    
+    @Test
+    public void testCommuteTime() throws Exception {
+    	assertTrue(general.getCommuteTime() == 20.00);
+    }
+    
+    @Test
+    public void testGetRemote() throws Exception {
+    	assertTrue(general.getRemote().equals(true));
+    }
+    
+    @Test
+    public void testgetsizeWeight() throws Exception {
+    	assertTrue(test1.getSizeWeight() == 5);
+    }
+    
+    @Test
+    public void testageWeight() throws Exception {
+    	assertTrue(test1.getAgeWeight() == 1);
+    }
+    
+    @Test
+    public void testcultureWeight() throws Exception {
+    	assertTrue(test1.getCultureWeight() == 5);
+    }
+    @Test
+    public void testopportunityWeight() throws Exception {
+    	assertTrue(test1.getSizeWeight() == 6);
+    }
+    
+    @Test
+    public void testgetworkLifeBalanceWeight() throws Exception {
+    	assertTrue(test1.getWorkLifeBalance() == 3);
+    }
+    
+    @Test
+    public void testgetsalaryWeight() throws Exception {
+    	assertTrue(general.getSalaryWeight() == 1);
+    }
 
     //test setter methods
+    
     @Test
     public void testSetSalary() throws Exception {
         general.setSalary(2020.20);
         assertTrue(general.getSalary() == 2020.20);
+    }
+    
+    @Test
+    public void testsetHousingStipendy() throws Exception {
+    	 general.setHousingStipend(1200);
+    	assertTrue(general.getHousingStipend() == 1200);
+    }
+    
+    @Test
+    public void testsetLocation() throws Exception {
+    	 general.setLocation("alaska");
+    	assertTrue(general.getLocation().contentEquals("alaska"));
+    }
+    
+    @Test
+    public void testsetCommuteTime() throws Exception {
+    	 general.setCommuteTime(35.00);
+    	assertTrue(general.getCommuteTime() == 35.00);
+    }
+    
+    @Test
+    public void testsetRemote() throws Exception {
+    	 general.setRemote(false);
+    	assertTrue(general.getRemote().equals(false));
+    }
+    
+    @Test
+    public void testsetsizeWeight() throws Exception {
+    	test1.setSizeWeight(6);
+    	assertTrue(test1.getSizeWeight() == 6);
+    }
+    
+    @Test
+    public void testsetageWeight() throws Exception {
+    	test1.setAgeWeight(6);
+    	assertTrue(test1.getAgeWeight() == 6);
+    }
+    
+    @Test
+    public void testsetcultureWeight() throws Exception {
+    	test1.setCultureWeight(1);
+    	assertTrue(test1.getCultureWeight() == 1);
+    }
+    @Test
+    public void testsetopportunityWeight() throws Exception {
+    	test1.setOpportunity(4);
+    	assertTrue(test1.getOpportunityWeight() == 4);
+    }
+    
+    @Test
+    public void testsetgetworkLifeBalanceWeight() throws Exception {
+    	test1.setWorkLifeBalance(3);
+    	assertTrue(test1.getWorkLifeBalance() == 3);
+    }
+    
+    @Test
+    public void testsetgetsalaryWeight() throws Exception {
+    	assertTrue(general.getSalaryWeight() == 1);
     }
 }
