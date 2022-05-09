f36e4837e05c563b821acb8e1ad23b1fb63e4bf0
bhageneder <98890401+bhageneder@users.noreply.github.com>
2022-05-08 21:55:59 -0400

    Update Queries
    Queries update

--- CS320_Team_Project/junit/database/QueriesTest.java
+++ CS320_Team_Project/junit/database/QueriesTest.java
@@ -1,16 +1,29 @@
 package database;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 import edu.ycp.cs320.teamProject.derbyDatabase.DatabaseProvider;
 import edu.ycp.cs320.teamProject.derbyDatabase.IDatabase;
+import edu.ycp.cs320.teamProject.derbyDatabase.Queries;
 
 public class QueriesTest {
 
 	private DatabaseProvider dbp;
 	private IDatabase idb;
+	private Queries query;
+	
+	@Test
+	public void testInsertUser()
+	{
+		query = new Queries();
+		String username = "username";
+		String password = "password";
+		int result = query.insertUser(username, password);
+		assertTrue(result == 1);
+	}
 	
 	@Test
 	public void testSetInstance()
